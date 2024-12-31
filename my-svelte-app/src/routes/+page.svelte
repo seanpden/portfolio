b641484559b912dba83e74fd973876ba1316a00c
seanpden <seanden522@gmail.com>
2024-12-31 13:20:12 -0600

    feat: implemented new group component

--- my-svelte-app/src/routes/+page.svelte
+++ my-svelte-app/src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import Group from '../components/Group.svelte';
 	import { onMount } from 'svelte';
-	import LeftGroup from '../components/LeftGroup.svelte';
-	import MainGroup from '../components/MainGroup.svelte';
 
 	let dt = $state(new Date().toLocaleString());
 	let headerheight = $state(0);
@@ -35,18 +33,22 @@
 	<!-- <div -->
 	<!-- 	class="mx-x my-y font-base mocha invisible grid grid-cols-[auto_1fr_auto] items-center bg-transparent font-mono text-text md:visible" -->
 	<!-- > -->
-	<Group group="home" class="col-auto min-w-32 justify-self-start">
+	<Group group="home" class="col-auto min-w-32 justify-self-start" content_class="w-fit">
 		<div>
 			<a class="hover:text-peach" href="/">seanpden.dev</a>
 		</div>
 	</Group>
-	<Group group="nav" class="col-auto grid w-full justify-self-center md:visible md:overflow-auto">
+	<Group
+		group="nav"
+		class="col-auto grid w-full justify-self-center md:visible md:overflow-auto"
+		content_class="w-fit"
+	>
 		<div>00. About</div>
 		<div>01. Experience</div>
 		<div>02. Project</div>
 		<div>03. Contact</div>
 	</Group>
-	<Group group="info" class="justify-self-end">
+	<Group group="info" class="justify-self-end" content_class="w-fit">
 		<div>
 			{dt}
 		</div>
@@ -57,23 +59,52 @@
 	style="height: calc(100vh - {headerheight}px);"
 	class="grid grid-cols-[auto_1fr_auto] text-text"
 >
-	<LeftGroup group="social" class="col-auto min-w-32 justify-self-start">
-		<div>
-			{'wide'}
+	<Group
+		group="social"
+		class="col-auto min-w-32 justify-self-start"
+		content_class="w-32 flex-col text-left "
+	>
+		<div class="self-start">
+			{'github'}
 		</div>
-	</LeftGroup>
-	<MainGroup group="main" class="col-auto w-full justify-self-center">
-		<div class="">
+		<div class="self-start">
+			{'instagram'}
+		</div>
+		<div class="self-start">
+			{'linkedin'}
+		</div>
+		<div class="self-start">
+			{'mastadon'}
+		</div>
+		<div class="self-start">
+			{'bluesky'}
+		</div>
+		<div class="self-start">
+			{'email'}
+		</div>
+	</Group>
+	<Group
+		group="main"
+		class="col-auto w-full justify-start justify-self-center"
+		content_class="w-full flex-col text-left"
+	>
+		<div class="self-start text-lg font-bold">
+			{'Lorem Ipsum'}
+		</div>
+		<div class="self-start">
+			{'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+		</div>
+		<div class="self-start">
+			{'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+		</div>
+		<div class="self-start">
 			{'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
 		</div>
-		<div class="">
+		<div class="self-start">
 			{'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
 		</div>
-		<div class="">
+		<div class="self-start">
 			{'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
 		</div>
-		<div class="">
-			{'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-		</div>
-	</MainGroup>
+	</Group>
 </div>
