debed04c898787202f1d16208d2ff4cd1594a047
seanpden <seanden522@gmail.com>
2024-12-31 13:19:57 -0600

    feat: abstracted the group component

--- my-svelte-app/src/components/Group.svelte
+++ my-svelte-app/src/components/Group.svelte
@@ -4,17 +4,21 @@
 		children: Snippet;
 		group: string;
 		class: string;
+		content_class: string;
 	};
 	let { children, group, ...rest }: Props = $props();
 </script>
 
-<div class="h-full w-fit bg-base p-2 pt-3 {rest.class}">
+<div class="h-full w-fit bg-transparent p-2 pt-3 {rest.class}">
 	<div
-		class="rounded-base h-full w-fit border border-overlay0 bg-base text-overlay0 transition-all hover:border-peach hover:text-peach {rest.class}"
+		class="rounded-base h-full w-fit border border-overlay0 bg-transparent text-overlay0 transition-all hover:border-mauve hover:text-mauve {rest.class}"
 	>
+		<!-- bg below should match root bg color -->
 		<div class="absolute z-10 mx-2 -mt-3.5 bg-base px-2 font-bold">{group}</div>
 		<!-- This is where each component differs... -->
-		<div class="rounded-base flex h-full w-fit items-center gap-4 bg-base p-2.5 text-text">
+		<div
+			class="rounded-base flex h-full items-center gap-4 bg-transparent p-2.5 text-text {rest.content_class}"
+		>
 			{@render children()}
 		</div>
 		<!-- end... -->
